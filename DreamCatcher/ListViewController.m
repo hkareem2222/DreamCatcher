8633b8e982365bf228830fdc94d52051f32d9678
Husein Kareem <huseinkareem@gmail.com>
2015-05-09 15:12:53 -0500

    displayed dreams on a list

--- DreamCatcher/ListViewController.m
+++ DreamCatcher/ListViewController.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+@property NSMutableArray *titlesArray;
+
 @end
 
 @implementation ListViewController
@@ -19,10 +21,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.titlesArray = [NSMutableArray new];
+
+}
+
+-(void) presentDreamEntry
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Enter New Dream" message:nil preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = @"Dream Title";
+
+    }];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        UITextField *textFieldOne = alertController.textFields[0];
+        [self.titlesArray addObject:textFieldOne.text];
+        [self.tableView reloadData];
+    }];
+
+    [alertController addAction:cancelAction];
+    [alertController addAction:saveAction];
+
+    [self presentViewController:alertController animated:true completion:nil];
 }
 
 - (IBAction)onAddButtonTapped:(UIBarButtonItem *)sender {
-
+    [self presentDreamEntry];
 }
 
 - (IBAction)onEditButtonTapped:(UIBarButtonItem *)sender {
@@ -31,13 +58,13 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return self.titlesArray.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-
+    cell.textLabel.text = [self.titlesArray objectAtIndex:indexPath.row];
     return cell;
 }
 
